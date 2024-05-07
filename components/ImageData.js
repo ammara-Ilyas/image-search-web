029534639e97104b049359c9c583d1c4963bc3b5
ammara-Ilyas <ismarailyas545@gmail.com>
2024-05-07 10:04:49 +0500

    Image search

--- components/ImageData.js
+++ components/ImageData.js
@@ -2,6 +2,7 @@
 import React, { useContext } from "react";
 import { AppContext } from "./contextApi/contextAPi";
 import style from "./style.module.css";
+import download from "@/image/download.png";
 function ImageData() {
   const { data, selectedOption } = useContext(AppContext);
   console.log("da", data);
@@ -16,20 +17,21 @@ function ImageData() {
           <img
             src={image.urls.small}
             alt={image.alt_description}
-            className="w-full h-full rounded"
+            className="w-full h-full rounded-md"
           />
           <a
             href={image.links.download}
-            className="absolute top-2 right-2 bg-white p-2 rounded-full shadow-lg"
+            target="_blank"
+            className="absolute bottom-2 right-2 bg-white p-1 rounded-full "
           >
             <img
-              src="@/image/download.png"
+              src={download.src}
               alt="Download"
-              className="w-6 h-6"
+              className="w-7 h-7 text-xl hover:text-red-300"
             />
           </a>
           <div
-            className={`${style.heading} absolute  bottom-full left-0 rounded-md w-full h-full flex justify-center items-center bg-black bg-opacity-50 text-white `}
+            className={`${style.heading} absolute  -top-full left-0 rounded-md w-full h-5/6 flex justify-center items-center bg-black bg-opacity-50 text-white `}
           >
             <h3 className="text-xl text-center"> {image.alt_description}</h3>
           </div>
